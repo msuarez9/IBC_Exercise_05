15e6e1615a065a8ad9ca34fd37a9e28a66ce8b09
Jake Fry <jfry1@nd.edu>
2019-10-09 18:51:01 -0400

    FINAL ANSWER TO #2

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 cat wages.csv | cut -d "," -f 1,2,4 | sed 's/,/ /g' | sort -k3,3n | tail -n 1
 
-cat wages.csv | cut -d "," -f 1,2,4 | sed 's/,/ /g' | sort -k3,3n | head -n 1
+cat wages.csv | cut -d "," -f 1,2,4 | sed 's/,/ /g' | sort -k3,3n | grep -v "gender yearsExperience"| head -n 1
 
 cat wages.csv | cut -d "," -f 1,2,4 | sed 's/,/ /g' | sort -k3,3n | tail -n 10 | grep -o "female" | wc -l 
+
