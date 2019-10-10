6eb8deb383a88584a867c3a59c41205655ed0c8f
Jake Fry <jfry1@nd.edu>
2019-10-10 16:02:11 -0400

    Answer to #2 with Headers

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
+echo "Lowest Earner"
 cat wages.csv | cut -d "," -f 1,2,4 | sed 's/,/ /g' | sort -k3,3n | tail -n 1
 
+echo "Highest Earner"
 cat wages.csv | cut -d "," -f 1,2,4 | sed 's/,/ /g' | sort -k3,3n | grep -v "gender yearsExperience"| head -n 1
 
+echo "# of Females in Top Ten Earners"
 cat wages.csv | cut -d "," -f 1,2,4 | sed 's/,/ /g' | sort -k3,3n | tail -n 10 | grep -o "female" | wc -l 
 
